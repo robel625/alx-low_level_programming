8a2c62d10323f569c61695709b1cb0a405fd3d0c
robel625 <76595851+robel625@users.noreply.github.com>
2021-11-27 04:20:44 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,15 +1,49 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - inserts a new node at the given position
- *
- * @h: pointer head
- * @idx: index
+ * insert_dnodeint_at_index - insert a node at idx position.
+ * @h: head of list
+ * @idx: index of node
  * @n: integer
- *
- * Return: the address of the new node or NULL if it fails
+ * Description: insert a node at idx position of double linked list.
+ * Return: node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new = NULL, *actual = *h;
+	unsigned int cont = 0;
 
+	if (h == NULL)
+		return (NULL);
+	if (h == NULL && idx != 0)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	while (actual != NULL)
+	{
+		if (idx == cont && actual->next != NULL)
+		{
+			new->next = actual;
+			new->prev = actual->prev;
+			actual->prev->next = new;
+			actual->prev = new;
+			return (new);
+		}
+		if (actual->next == NULL && cont == idx)
+		{
+			actual->next = new;
+			new->prev = actual;
+			return (new);
+		}
+		cont++;
+		actual = actual->next;
+	}
+	return (NULL);
 }
